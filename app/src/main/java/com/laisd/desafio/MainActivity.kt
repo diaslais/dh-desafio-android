14f43eeccce1b1b58ba3192786687de63d42a02b
Lais Dias <laisdias099@gmail.com>
2020-11-11 22:03:20 -0300

    criando intents para telas de register e home

--- app/src/main/java/com/laisd/desafio/MainActivity.kt
+++ app/src/main/java/com/laisd/desafio/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.laisd.desafio
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val loginBtnLogin = findViewById<Button>(R.id.loginBtnLogIn)
+        val loginBtnRegister = findViewById<Button>(R.id.loginBtnRegister)
+
+        loginBtnLogin.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        loginBtnRegister.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
