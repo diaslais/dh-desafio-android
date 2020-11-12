e20a30715902ebf3126cc1c34ec88d888b064d35
Lais Dias <laisdias099@gmail.com>
2020-11-12 01:27:53 -0300

    implementando recyclerview de restaurantes na tela de Home

--- app/src/main/java/com/laisd/desafio/HomeActivity.kt
+++ app/src/main/java/com/laisd/desafio/HomeActivity.kt
@@ -2,10 +2,30 @@ package com.laisd.desafio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val homeViewManager = LinearLayoutManager(this)
+        val homeRecyclerView = findViewById<RecyclerView>(R.id.homeRecyclerView)
+
+        val homeRestaurantesDataSet = arrayListOf<Restaurante>(
+            Restaurante(R.drawable.tonysroma, "Tony Roma's", "Av. Lavandisca, 717 - Indianópolis, São Paulo", "Fecha às 22:00"),
+            Restaurante(R.drawable.aoyamamoema, "Aoyama - Moema", "Alameda dos Arapanés, 532 - Moema", "Fecha às 00:00"),
+            Restaurante(R.drawable.outbackmoema, "Outback - Moema", "Av. Moaci, 187, 187 - Moema, São Paulo", "Fecha às 00:00"),
+            Restaurante(R.drawable.sisenor, "Sí Señor!", "Alameda Jauaperi, 626 - Moema", "Fecha às 01:00")
+        )
+
+        val homeViewAdapter = HomeRecyclerviewAdapter(homeRestaurantesDataSet)
+
+        homeRecyclerView.apply {
+            layoutManager = homeViewManager
+            adapter = homeViewAdapter
+        }
+
     }
 }
--- app/src/main/java/com/laisd/desafio/HomeRecyclerviewAdapter.kt
+++ app/src/main/java/com/laisd/desafio/HomeRecyclerviewAdapter.kt
@@ -1,5 +1,6 @@
 package com.laisd.desafio
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -7,24 +8,30 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import org.w3c.dom.Text
 
-class HomeRecyclerviewAdapter: RecyclerView.Adapter<HomeRecyclerviewAdapter.HomeViewHolder>() {
+class HomeRecyclerviewAdapter(private val homeDataSet: List<Restaurante>): RecyclerView.Adapter<HomeRecyclerviewAdapter.HomeViewHolder>() {
 
     class HomeViewHolder(view: View): RecyclerView.ViewHolder(view) {
         private var fotoHome = view.findViewById<ImageView>(R.id.imgHome)
         private var nomeHome = view.findViewById<TextView>(R.id.txtHomeNome)
         private var enderecoHome = view.findViewById<TextView>(R.id.txtHomeEndereco)
         private var horarioHome = view.findViewById<TextView>(R.id.txtHomeHorario)
+
+        fun bind(restaurante: Restaurante){
+            fotoHome.setImageResource(restaurante.foto)
+            nomeHome.text = restaurante.nome
+            enderecoHome.text = restaurante.endereco
+            horarioHome.text = restaurante.horario
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_home_recyclerview, parent, false)
+        return HomeViewHolder(view)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount() = homeDataSet.size
 
     override fun onBindViewHolder(holder: HomeViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(homeDataSet[position])
     }
 }
