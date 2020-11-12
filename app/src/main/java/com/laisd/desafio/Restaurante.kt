ecd82cf1a61c96343ae03ef1dbb5adc9d148c0b6
Lais Dias <laisdias099@gmail.com>
2020-11-12 01:28:57 -0300

    criando data class restaurante

---
+++ app/src/main/java/com/laisd/desafio/Restaurante.kt
@@ -0,0 +1,11 @@
+package com.laisd.desafio
+
+import android.widget.ImageView
+import android.widget.TextView
+
+data class Restaurante (
+    val foto: Int,
+    val nome: String,
+    val endereco: String,
+    val horario: String
+)
