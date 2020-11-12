0d2caeec58014f6eb6978b891a6a0b342aacf778
Lais Dias <laisdias099@gmail.com>
2020-11-12 02:20:53 -0300

    validar campos vazios na tela de login

--- app/src/main/java/com/laisd/desafio/MainActivity.kt
+++ app/src/main/java/com/laisd/desafio/MainActivity.kt
@@ -3,7 +3,11 @@ package com.laisd.desafio
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,8 +18,49 @@ class MainActivity : AppCompatActivity() {
         val loginBtnRegister = findViewById<Button>(R.id.loginBtnRegister)
 
         loginBtnLogin.setOnClickListener {
-            val intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
+            val loginEmail = findViewById<TextInputEditText>(R.id.loginTxtEdtEmail)
+            val loginEmailLayout = findViewById<TextInputLayout>(R.id.loginTxtLayoutEmail)
+            val loginPassword = findViewById<TextInputEditText>(R.id.loginTxtEdtPassword)
+            val loginPasswordLayout = findViewById<TextInputLayout>(R.id.loginTxtLayoutPassword)
+
+            var taSemErro = true
+
+            if (loginEmail.text.toString().isEmpty()){
+                loginEmailLayout.error = "Email is required"
+                taSemErro = false
+            }
+            loginEmail.addTextChangedListener(object: TextWatcher {
+                override fun afterTextChanged(p0: Editable?) {
+                }
+
+                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                }
+
+                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    loginEmailLayout.error = ""
+                }
+            })
+
+            if (loginPassword.text.toString().isEmpty()){
+                loginPasswordLayout.error = "Password is required"
+                taSemErro = false
+            }
+            loginPassword.addTextChangedListener(object: TextWatcher {
+                override fun afterTextChanged(p0: Editable?) {
+                }
+
+                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                }
+
+                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    loginPasswordLayout.error = ""
+                }
+            })
+
+            if (taSemErro) {
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+            }
         }
 
         loginBtnRegister.setOnClickListener {
