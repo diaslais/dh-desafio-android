c61d73c0cc46945292386b02aaef622e781a1916
Lais Dias <laisdias099@gmail.com>
2020-11-12 21:46:57 -0300

    Adapter da recyclerview da tela de Pratos

---
+++ app/src/main/java/com/laisd/desafio/PratosRecyclerviewAdapter.kt
@@ -0,0 +1,33 @@
+package com.laisd.desafio
+
+import android.media.Image
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class PratosRecyclerviewAdapter (private val pratosDataSet: List<Prato>): RecyclerView.Adapter<PratosRecyclerviewAdapter.PratosViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PratosViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_prato_recyclerview, parent, false)
+        return PratosRecyclerviewAdapter.PratosViewHolder(view)
+    }
+
+    override fun getItemCount() = pratosDataSet.size
+
+    override fun onBindViewHolder(holder: PratosViewHolder, position: Int) {
+        holder.bind(pratosDataSet[position])
+    }
+
+    class PratosViewHolder(view: View): RecyclerView.ViewHolder(view){
+        private var fotoDetalhe= view.findViewById<ImageView>(R.id.imgDetalheCardapio)
+        private var nomeDetalhe = view.findViewById<TextView>(R.id.txtNomeDetalheCardapio)
+
+        fun bind(prato: Prato){
+            fotoDetalhe.setImageResource(prato.foto)
+            nomeDetalhe.text = prato.nome
+        }
+    }
+}
